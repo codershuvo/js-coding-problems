781375a2119aea6bd860548673e0cc91e688a121
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-19 02:51:34 +0600

    swap

---
+++ swap.js
@@ -0,0 +1,22 @@
+var a = 5; 
+var b = 7; 
+// console.log('before swap: a=', a, 'b=', b); 
+var temp = a; 
+a = b; 
+b = temp; 
+// console.log('after swap: a=', a, 'b=', b); 
+
+
+var x = 5; 
+var y = 7; 
+// console.log('before swap', x, y); 
+x = x + y; 
+y = x - y; 
+x = x - y; 
+// console.log('after swap', x, y); 
+
+
+var p = 5; 
+var q = 7; 
+[p, q] = [q, p]; 
+console.log('after swap', p, q); 
