859145ceeed4dfc3be1f80fddfce20bcfaf101a2
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-19 03:29:48 +0600

    array sum

---
+++ arraySum.js
@@ -0,0 +1,11 @@
+function getArrayTotal (numbers) {
+var sum = 0; 
+    for (var i = 0; i < numbers.length; i++) {
+        var element = numbers[i]; 
+        sum = sum + element; 
+    }
+    return sum; 
+}
+var result = [45, 65, 56, 45, 12, 3, 54, 69, 71]; 
+var output = getArrayTotal(result); 
+console.log(output); 
