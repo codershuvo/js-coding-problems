65a28d2d0ef6288bc834d9a1d63d40aa6d110d7b
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-19 03:11:48 +0600

    max number

---
+++ max.js
@@ -0,0 +1,23 @@
+var business = 850; 
+var minister = 550; 
+var sochib = 950; 
+
+// if (business > minister) {
+//     if (business > sochib) {
+//         console.log('business is bigger'); 
+//     }
+//     else {
+//         console.log('sochib is bigger'); 
+//     }
+// }
+// else {
+//     if (minister > sochib) {
+//         console.log('minister is bigger'); 
+//     }
+//     else {
+//         console.log('sochib is bigger'); 
+//     }
+// }
+
+var max = Math.max(business, minister, sochib); 
+console.log(max); 
